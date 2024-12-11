3e660f8cca0443f75b618dc3ccbd34b6e2861bc3
Lucas Nunes Carvalho <lucasnc10@hotmail.com>
2024-12-11 09:22:13 -0300

    prisma config, register page, claimUserNameForm

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles';
+import Image from 'next/image'
+import previewImage from '../../assets/preview.png'
+import { ClaimUsernameForm } from '../components/ClaimUserNameForm';
+
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+        <Text size='lg'>Conecte seu calendário e permita que as
+          pessoas marquem agendamentos no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt='Calendário simbolizando aplicação em funcionamento'
+        />
+      </Preview>
+    </Container>
+  );
+}
